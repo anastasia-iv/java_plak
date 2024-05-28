d4825d0897b0f75e6721a84ce773460b58622c89
a.ivanova <ivanovaaa@my.msu.ru>
2024-05-28 16:16:15 +0300

    trying to fix

--- scheduler/src/main/java/ru/msu/cmc/scheduler/controllers/StudentController.java
+++ scheduler/src/main/java/ru/msu/cmc/scheduler/controllers/StudentController.java
@@ -65,7 +65,7 @@ public class StudentController {
     public String editStudentPage(@RequestParam(name = "studentId", required = false) Integer studentId, Model model) {
         Student student;
         if (studentId == null) {
-            student = new Student();
+            student = new Student(0, "No name", 3, 300, 3, 4.56F, "");
         } else {
             student = studentDAO.getById(studentId);
             if (student == null) {
@@ -79,22 +79,21 @@ public class StudentController {
     }
 
     @PostMapping("/saveStudent")
-    public String saveStudentPage(@RequestParam(name = "studentId") Integer studentId,
-                                  @RequestParam(name = "name") String name,
-                                  @RequestParam(name = "flow") Integer flow,
-                                  @RequestParam(name = "group") Integer group,
-                                  @RequestParam(name = "year") Integer year,
-                                  @RequestParam(name = "average") Float average,
-                                  @RequestParam(name = "portfolio", required = false) String portfolio,
-                                  Model model) {
+    public String saveStudent(@RequestParam(name = "studentId") Integer studentId,
+                              @RequestParam(name = "name") String name,
+                              @RequestParam(name = "flow") Integer flow,
+                              @RequestParam(name = "group") Integer group,
+                              @RequestParam(name = "year") Integer year,
+                              @RequestParam(name = "average") Float average,
+                              @RequestParam(name = "portfolio", required = false) String portfolio,
+                              Model model) {
         Logger logger = LoggerFactory.getLogger(HomeController.class);
 
         logger.info("Saving student with id: {}, name: {}, flow: {}, group: {}, year: {}, average: {}, portfolio: {}",
                 studentId, name, flow, group, year, average, portfolio);
 
-        Student student = studentDAO.getById(studentId);
-
-        if (studentId != null) {
+        Student student;
+        if (studentId != 0) {
             student = studentDAO.getById(studentId);
             if (student == null) {
                 model.addAttribute("error_msg", "В базе нет студента с ID = " + studentId);
@@ -111,14 +110,13 @@ public class StudentController {
         student.setAverage(average);
         student.setPortfolio(portfolio);
 
-        if (studentId != null) {
+        if (studentId != 0) {
             studentDAO.update(student); // Сохранение изменений для существующего студента
         } else {
             studentDAO.save(student); // Сохранение нового студента
         }
 
-//        return "redirect:/student?studentId=" + student.getId();
-        return "redirect:/students";
+        return "redirect:/student?studentId=" + student.getId();
     }
 
 //    @PostMapping("/removeStudent")
